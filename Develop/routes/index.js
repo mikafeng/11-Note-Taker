1b4197de3b1bd5278907d47abdf11ad007e4936a
mikafeng <mikafengler@gmail.com>
2023-03-03 15:35:19 -0500

    deleted routes, using server.js exclusively to route

--- Develop/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const notesRouter = require('./notes')
-
-const app = express();
-
-app.use('/api', notesRouter);
-
-module.exports = app;
--- Develop/server.js
+++ Develop/server.js
@@ -1,24 +1,38 @@
-const express = require('express');
-const fs = require('fs');
-const api = require('./routes/index.js');
-
-
-const PORT = process.env.PORT || 3001;
+//dependencies
+const express = require("express");
+const fs = require("fs");
+// const api = require("../routes/htmlRoute.js");
+const database = require("./db/db.json")
+//sets up express server and initial port
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+
+//middleware
+//sets up the Express app to handle data parsing for POST and PUT requests
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+//sets up the Express app to serve static files
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
+
+//router
+//points server to router files
+
+app.get('*', (req, res) =>
     res.sendFile(`${__dirname}`, '/public/index.html')
 );
 
 app.get('/notes', (req, res) => {
     res.sendFile(`${__dirname}`, '/public/notes.html')
 })
-app.use('/', api);
+
+
+
+// app.use('/', api);
+
+
 
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT} `)

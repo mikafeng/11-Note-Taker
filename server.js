9a40b0a54b9c3a9e58018208a48d76cbc5876cdb
mikafeng <mikafengler@gmail.com>
2023-03-14 16:47:20 -0400

    add post route and object properites for json data

--- server.js
+++ server.js
@@ -22,13 +22,30 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-
-// GET/notes return notes.html
-// GET * return indexedDB.html
-
 //API ROUTES
 app.get('/api/notes', (req, res) => {
     res.json(noteData);
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add new note`);
+
+    // destructure for items in req.body
+    const {title, text} = req.body;
+    //if all properties are present
+    if(title && text) {
+        //variable for object that is saved
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        };
+
+        console.log(response);
+        res.status(201).json(response);
+    } else {
+        res.status(500).json('Error in saving note')
+    }
 });
 
 // GET /api/notes db.json
@@ -37,6 +54,7 @@ app.get('/api/notes', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './publi/index.html'))
 });
+
 
 app.listen(PORT, () =>
 console.log(`Application now listening at http://localhost:${PORT}`)
